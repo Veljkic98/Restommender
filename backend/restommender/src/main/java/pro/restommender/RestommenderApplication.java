c51cfe615c1fa832ba37a2ff59d89887f50d3e48
alekalek <alek@spectra-alarm.com>
2021-05-14 09:39:13 +0200

    initialized backend

---
+++ backend/restommender/src/main/java/pro/restommender/RestommenderApplication.java
@@ -0,0 +1,13 @@
+package pro.restommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestommenderApplication.class, args);
+	}
+
+}
