e1a7d1aaa0988624ef9add6cb040ad3775461659
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-05-21 01:49:05 +0200

    drugi pokusaj

--- backend/drools-spring-app/src/main/java/com/flowershop/FlowershopApp.java
+++ backend/drools-spring-app/src/main/java/com/flowershop/FlowershopApp.java
@@ -1,47 +1,61 @@
 package com.flowershop;
 
+import java.util.Arrays;
+
 import com.flowershop.model.Message;
 
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class FlowershopApp {
 
+	private static Logger log = LoggerFactory.getLogger(FlowershopApp.class);
+
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(FlowershopApp.class, args);
 
-		try {
-            // load up the knowledge base
-	        KieServices ks = KieServices.Factory.get();  // instanciranje rule engine-a
-    	    KieContainer kContainer = ks.getKieClasspathContainer();  // generisemo kie kontejner, na osnovu kie servisa
-        	KieSession kSession = kContainer.newKieSession("ksession-rules"); //iz kie kontejnera dobijamo novu kie sesiju
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
 
-            // go ! 
-            Message message = new Message();
-            message.setMessage("Hello World");
-            message.setStatus(Message.HELLO);
-            kSession.insert(message);  // ubacujemo cinjenicu u radnu memoriju
-            int firedRules = kSession.fireAllRules();  // aktiviramo pravilo
+		StringBuilder sb = new StringBuilder("Application beans:\n");
+		for (String beanName : beanNames) {
+			sb.append(beanName + "\n");
+		}
+		log.info(sb.toString());
 
-			System.out.println("--------------------------");
-			System.out.println("number of fired rules: " + firedRules);
+		Message message = new Message();
+		message.setMessage("Hello World");
+		message.setStatus(Message.HELLO);
 
-			kSession.getAgenda().getAgendaGroup("promotions").setFocus();
-			firedRules = kSession.fireAllRules();
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieSession kieSession = kContainer.newKieSession();
+		kieSession.insert(message);
+		int num = kieSession.fireAllRules();
+		kieSession.dispose();
 
-			System.out.println("--------------------------");
-			System.out.println("number of fired rules: " + firedRules);
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
+		System.out.println("----------------------");
+		System.out.println("Fired rules: " + num);
 	}
 
+	@Bean
+	public KieContainer kieContainer() {
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks
+				.newKieContainer(ks.newReleaseId("sbnz.integracija", "drools-spring-kjar", "0.0.1-SNAPSHOT"));
+		KieScanner kScanner = ks.newKieScanner(kContainer);
+		kScanner.start(10_000);
+		return kContainer;
+	}
 
 }
