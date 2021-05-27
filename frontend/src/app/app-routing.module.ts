3b6190da1277d650f81e51efafd80f7dd3034075
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-05-28 00:11:52 +0200

    added home page and toolbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './componenents/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    children: [
+      // {
+      //   path: 'login',
+      //   component: LoginComponent,
+      //   canActivate: [LoginGuard]
+      // },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
