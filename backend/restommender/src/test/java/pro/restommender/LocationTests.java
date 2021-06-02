bba18df337907bb437ec3139bbd0710478e48608
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-06-02 20:03:24 +0200

    Update LocationTests.java

--- backend/restommender/src/test/java/pro/restommender/LocationTests.java
+++ backend/restommender/src/test/java/pro/restommender/LocationTests.java
@@ -13,16 +13,13 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import pro.restommender.dto.Search;
 import pro.restommender.model.RelevantRestaurants;
 import pro.restommender.model.Restaurant;
 import pro.restommender.repository.RestaurantRepository;
 
-// @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource("classpath:test.properties")
 public class LocationTests {
@@ -159,6 +156,60 @@ public class LocationTests {
 
 		Search s = new Search();
 		s.setLocation(0);
+
+		List<Restaurant> restaurants = restaurantRepository.findAll();
+
+		RelevantRestaurants rr = new RelevantRestaurants();
+		rr.setRelevantRestaurants(restaurants);
+
+		kieSession.getAgenda().getAgendaGroup("location").setFocus();
+		kieSession.insert(rr);
+		kieSession.insert(s);
+		int num = kieSession.fireAllRules();
+
+		kieSession.dispose();
+
+		System.out.println("Fired rules: " + num);
+		System.out.println(rr.getRelevantRestaurants().size());
+
+		assertTrue(rr.getRelevantRestaurants().size() == 1);
+		assertEquals(num, 1);
+	}
+
+	@Test
+	void searchLocation_location1_true() {
+
+		KieSession kieSession = kContainer.newKieSession("ksession-rules");
+
+		Search s = new Search();
+		s.setLocation(1);
+
+		List<Restaurant> restaurants = restaurantRepository.findAll();
+
+		RelevantRestaurants rr = new RelevantRestaurants();
+		rr.setRelevantRestaurants(restaurants);
+
+		kieSession.getAgenda().getAgendaGroup("location").setFocus();
+		kieSession.insert(rr);
+		kieSession.insert(s);
+		int num = kieSession.fireAllRules();
+
+		kieSession.dispose();
+
+		System.out.println("Fired rules: " + num);
+		System.out.println(rr.getRelevantRestaurants().size());
+
+		assertTrue(rr.getRelevantRestaurants().size() == 1);
+		assertEquals(num, 1);
+	}
+
+	@Test
+	void searchLocation_location5_true() {
+
+		KieSession kieSession = kContainer.newKieSession("ksession-rules");
+
+		Search s = new Search();
+		s.setLocation(5);
 
 		List<Restaurant> restaurants = restaurantRepository.findAll();
 
