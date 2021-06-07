13adc24b32bd051acc0439ad0e20309409e7f28d
alekalek <alek@spectra-alarm.com>
2021-06-07 16:07:27 +0200

    front done

---
+++ frontend/src/app/model/restaurant.model.ts
@@ -0,0 +1,13 @@
+export interface Restaurant {
+  id: number,
+  name : string,
+  location : number,
+  music : string,
+  accomodation : string,
+  smokingArea : boolean,
+  nonSmokingArea : boolean,
+  alcoholicDrinks : boolean,
+  nonAlcoholicDrinks : boolean,
+  petFriendly : boolean,
+  kidFriendly : boolean,
+}
