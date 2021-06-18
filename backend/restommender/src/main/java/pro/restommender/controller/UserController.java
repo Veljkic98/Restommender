0d2de8ac8b29dd4b6e30d34104073386740196bb
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-06-18 21:26:36 +0200

    clean and uncomment some code and authorisations

--- backend/restommender/src/main/java/pro/restommender/controller/UserController.java
+++ backend/restommender/src/main/java/pro/restommender/controller/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import pro.restommender.dto.responseDTO.UserResponseDTO;
 import pro.restommender.mapper.UserMapper;
 import pro.restommender.model.AuthenticatedUser;
+import pro.restommender.model.User;
 import pro.restommender.service.UserService;
 
 @RestController
@@ -26,17 +29,16 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    //TODO: na kraju otkomentarisati zakomentarisano
     @GetMapping(path = "blocked")
-    // @PreAuthorize("hasRole('ROLE_AUTH_USER')")
+    @PreAuthorize("hasRole('ROLE_AUTH_USER')")
     public ResponseEntity<?> getAllBlocked() {
 
         try {
 
-            // User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-            // if (user.getType().name().equals("USER"))
-            //     return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
+            if (user.getType().name().equals("USER"))
+                return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
 
             List<AuthenticatedUser> users = userService.getAll(true);
 
@@ -49,16 +51,15 @@ public class UserController {
         }
     }
 
-    //TODO: na kraju otkomentarisati zakomentarisano
     @GetMapping(path = "unblocked")
-    // @PreAuthorize("hasRole('ROLE_AUTH_USER')")
+    @PreAuthorize("hasRole('ROLE_AUTH_USER')")
     public ResponseEntity<?> getAllUnblocked() {
 
         try {
-            // User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-            // if (user.getType().name().equals("USER"))
-            //     return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
+            if (user.getType().name().equals("USER"))
+                return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
 
             List<AuthenticatedUser> users = userService.getAll(false);
 
@@ -72,14 +73,14 @@ public class UserController {
     }
 
     @PutMapping(path = "unblock/{userId}")
-    // @PreAuthorize("hasRole('ROLE_AUTH_USER')")
+    @PreAuthorize("hasRole('ROLE_AUTH_USER')")
     public ResponseEntity<?> unblock(@PathVariable Long userId) {
 
         try {
-            // User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-            // if (user.getType().name().equals("USER"))
-            //     return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
+            if (user.getType().name().equals("USER"))
+                return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
 
             userService.unblock(userId);
             
@@ -91,14 +92,14 @@ public class UserController {
     }
 
     @PutMapping(path = "block/{userId}")
-    // @PreAuthorize("hasRole('ROLE_AUTH_USER')")
+    @PreAuthorize("hasRole('ROLE_AUTH_USER')")
     public ResponseEntity<?> block(@PathVariable Long userId) {
 
         try {
-            // User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-            // if (user.getType().name().equals("USER"))
-            //     return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
+            if (user.getType().name().equals("USER"))
+                return new ResponseEntity<>("User must be admin.", HttpStatus.UNAUTHORIZED);
 
             userService.block(userId);
             
