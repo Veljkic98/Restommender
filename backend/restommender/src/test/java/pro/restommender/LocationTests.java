9736e02ab15b5a98cfff7813262bb55cf0ff097b
Veljko <51533885+Veljko998@users.noreply.github.com>
2021-06-18 21:35:30 +0200

    all test pass

--- backend/restommender/src/test/java/pro/restommender/LocationTests.java
+++ backend/restommender/src/test/java/pro/restommender/LocationTests.java
@@ -3,7 +3,6 @@ package pro.restommender;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
